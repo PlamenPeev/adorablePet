b6ec3092188cbde90ab845e1bc2330666c2b7832
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-01-25 22:28:30 +0200

    Initial commit

---
+++ src/main/java/com/example/adorablepet/models/aaa.java
@@ -0,0 +1,2 @@
+package com.example.adorablepet.models;public class aaa {
+}
---
+++ src/test/java/com/example/adorablepet/AdorablePetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adorablepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdorablePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
