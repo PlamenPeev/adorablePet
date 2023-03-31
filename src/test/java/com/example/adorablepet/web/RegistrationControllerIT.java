291f0d67caf5de34eb77320af604f9fe32706a11
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-03-31 22:51:29 +0300

    completed one integration test

--- src/test/java/com/example/adorablepet/web/RegistrationControllerIT.java
+++ src/test/java/com/example/adorablepet/web/RegistrationControllerIT.java
@@ -1,6 +1,6 @@
 package com.example.adorablepet.web;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,7 +38,7 @@ public class RegistrationControllerIT {
                         .with(csrf())
                 ).
                 andExpect(status().is3xxRedirection()).
-                andExpect(redirectedUrl("/users/login"));
+                andExpect(redirectedUrl("login"));
     }
 
 }
