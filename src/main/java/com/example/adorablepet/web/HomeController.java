6be9f9be1dbefa9e37d2843d637480b7a5021da0
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-04-02 11:28:17 +0300

    reorganize HomeController

--- src/main/java/com/example/adorablepet/web/HomeController.java
+++ src/main/java/com/example/adorablepet/web/HomeController.java
@@ -1,87 +1,95 @@
 package com.example.adorablepet.web;
 
-import com.example.adorablepet.models.entities.Pet;
-import com.example.adorablepet.models.entities.UserEntity;
 import com.example.adorablepet.models.enums.TypeOfHelpEnumName;
 import com.example.adorablepet.models.views.PetViewModel;
-import com.example.adorablepet.repository.PetRepository;
 import com.example.adorablepet.service.PetService;
-import com.example.adorablepet.service.UserService;
-import com.example.adorablepet.session.CurrentUser;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
+@Controller
+public class HomeController {
 
-//@Controller
-//@RequestMapping("pages")
-//public class HomeController {
-//
-//    private final PetService petService;
-//    private final UserService userService;
-//    private final PetRepository petRepository;
-//    private final CurrentUser currentUser;
-//
-//    public HomeController(PetService petService, UserService userService, PetRepository petRepository, CurrentUser currentUser) {
-//        this.petService = petService;
-//        this.userService = userService;
-//        this.petRepository = petRepository;
-//        this.currentUser = currentUser;
-//    }
-//
-//    @GetMapping("/my-pets")
-//    public String index(Model model) {
-//
-////        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-////
-////        if (principal.getUsername() == null) {
-////            return "index";
-////        }
-//
-//      //
-////
-////        if (user.getId() == null) {
-////            return "index";
-////        }
-//
-//        if (currentUser.getId() == null) {
-//            return "index";
-//        }
-//
-//        String userMail = currentUser.getEmail();
-//
-//
-//        List<Pet> allPets = this.petRepository.findAll();
-//        List<Pet> petsByUser = this.petRepository.findUserByEmail(userMail);
-//
-//
-//        List<PetViewModel> treatments = this.petService.findAllByTypeOfHelp(TypeOfHelpEnumName.TREATMENT);
-//        List<PetViewModel> grooms = this.petService.findAllByTypeOfHelp(TypeOfHelpEnumName.GROOMING);
-//        List<PetViewModel> hotels = this.petService.findAllByTypeOfHelp(TypeOfHelpEnumName.HOTEL);
-//        List<PetViewModel> schools = this.petService.findAllByTypeOfHelp(TypeOfHelpEnumName.SCHOOL);
-//        List<PetViewModel> preventions = this.petService.findAllByTypeOfHelp(TypeOfHelpEnumName.PREVENTION);
-//
-//
-//
-//        model.addAttribute("treatments", treatments);
-//        model.addAttribute("grooms", grooms);
-//        model.addAttribute("hotels", hotels);
-//        model.addAttribute("schools", schools);
-//        model.addAttribute("preventions", preventions);
-//        model.addAttribute("totalCount", allPets
-//                .stream()
-//                .map(Pet::getAge)
-//                .reduce(Double::sum)
-//                .orElse(Double.valueOf(0.0)));
-//        model.addAttribute("petsByUser", petsByUser);
-//
-//        return "pages/my-pets";
-//    }
+    private final PetService petService;
+
+    public HomeController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @GetMapping("/pages/my-pets")
+
+    public String MyPets(Model model, Principal principal) {
+
+        if (principal.getName() == null) {
+            return "index";
+        }
+
+        List<PetViewModel> treatments = this.petService.findPetsByUsername(principal.getName(), TypeOfHelpEnumName.TREATMENT);
+        List<PetViewModel> grooms = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.GROOMING);
+        List<PetViewModel> hotels = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.HOTEL);
+        List<PetViewModel> schools = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.SCHOOL);
+        List<PetViewModel> preventions = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.PREVENTION);
+        Long numVisitByUser = this.petService.findPetsByOwner(principal.getName());
+
+        model.addAttribute("treatments", treatments);
+        model.addAttribute("grooms", grooms);
+        model.addAttribute("hotels", hotels);
+        model.addAttribute("schools", schools);
+        model.addAttribute("preventions", preventions);
+        model.addAttribute("numVisitByUser", numVisitByUser);
+        model.addAttribute("user", principal.getName());
 
 
-//}
+        return "pages/my-pets";
+    }
+
+    @GetMapping("/pages/moderators")
+    private String pageModerators(Model model){
+
+
+        List<PetViewModel> treatments = this.petService.findPets(TypeOfHelpEnumName.TREATMENT);
+        List<PetViewModel> grooms = this.petService.findPets(TypeOfHelpEnumName.GROOMING);
+        List<PetViewModel> hotels = this.petService.findPets(TypeOfHelpEnumName.HOTEL);
+        List<PetViewModel> schools = this.petService.findPets(TypeOfHelpEnumName.SCHOOL);
+        List<PetViewModel> preventions = this.petService.findPets(TypeOfHelpEnumName.PREVENTION);
+        Long countOfPets = this.petService.countAllPets();
+
+        model.addAttribute("treatments", treatments);
+        model.addAttribute("grooms", grooms);
+        model.addAttribute("hotels", hotels);
+        model.addAttribute("schools", schools);
+        model.addAttribute("preventions", preventions);
+        model.addAttribute("countOfPets", countOfPets);
+
+
+        return "pages/moderators";
+    }
+
+    @GetMapping("/pages/admins")
+    public String pageAdmins(Model model) {
+
+
+        List<PetViewModel> treatments = this.petService.findPets(TypeOfHelpEnumName.TREATMENT);
+        List<PetViewModel> grooms = this.petService.findPets(TypeOfHelpEnumName.GROOMING);
+        List<PetViewModel> hotels = this.petService.findPets(TypeOfHelpEnumName.HOTEL);
+        List<PetViewModel> schools = this.petService.findPets(TypeOfHelpEnumName.SCHOOL);
+        List<PetViewModel> preventions = this.petService.findPets(TypeOfHelpEnumName.PREVENTION);
+        Long countOfPets = this.petService.countAllPets();
+
+
+        model.addAttribute("treatments", treatments);
+        model.addAttribute("grooms", grooms);
+        model.addAttribute("hotels", hotels);
+        model.addAttribute("schools", schools);
+        model.addAttribute("preventions", preventions);
+        model.addAttribute("countOfPets", countOfPets);
+
+
+
+        return "pages/admins";
+    }
+
+}
--- src/main/java/com/example/adorablepet/web/PagesController.java
+++ src/main/java/com/example/adorablepet/web/PagesController.java
@@ -1,19 +1,13 @@
 package com.example.adorablepet.web;
 
 import com.example.adorablepet.models.entities.ObjectNotFoundException;
-import com.example.adorablepet.models.enums.TypeOfHelpEnumName;
-import com.example.adorablepet.models.views.PetViewModel;
 import com.example.adorablepet.repository.PetRepository;
 import com.example.adorablepet.service.PetService;
 import com.example.adorablepet.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequestMapping("/pages")
@@ -70,83 +64,6 @@ public class PagesController {
     private String projects(){
         return "pages/projects";
     }
-
-
-    @GetMapping("/my-pets")
-
-    public String MyPets(Model model,Principal principal) {
-
-        if (principal.getName() == null) {
-            return "index";
-        }
-
-        List<PetViewModel> treatments = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.TREATMENT);
-        List<PetViewModel> grooms = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.GROOMING);
-        List<PetViewModel> hotels = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.HOTEL);
-        List<PetViewModel> schools = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.SCHOOL);
-        List<PetViewModel> preventions = this.petService.findPetsByUsername(principal.getName(),TypeOfHelpEnumName.PREVENTION);
-        Long numVisitByUser = this.petService.findPetsByOwner(principal.getName());
-
-        model.addAttribute("treatments", treatments);
-        model.addAttribute("grooms", grooms);
-        model.addAttribute("hotels", hotels);
-        model.addAttribute("schools", schools);
-        model.addAttribute("preventions", preventions);
-        model.addAttribute("numVisitByUser", numVisitByUser);
-        model.addAttribute("user", principal.getName());
-
-
-        return "pages/my-pets";
-    }
-
-    @GetMapping("/moderators")
-    private String pageModerators(Model model){
-
-
-        List<PetViewModel> treatments = this.petService.findPets(TypeOfHelpEnumName.TREATMENT);
-        List<PetViewModel> grooms = this.petService.findPets(TypeOfHelpEnumName.GROOMING);
-        List<PetViewModel> hotels = this.petService.findPets(TypeOfHelpEnumName.HOTEL);
-        List<PetViewModel> schools = this.petService.findPets(TypeOfHelpEnumName.SCHOOL);
-        List<PetViewModel> preventions = this.petService.findPets(TypeOfHelpEnumName.PREVENTION);
-        Long countOfPets = this.petService.countAllPets();
-
-        model.addAttribute("treatments", treatments);
-        model.addAttribute("grooms", grooms);
-        model.addAttribute("hotels", hotels);
-        model.addAttribute("schools", schools);
-        model.addAttribute("preventions", preventions);
-        model.addAttribute("countOfPets", countOfPets);
-
-
-
-        return "pages/moderators";
-    }
-
-    @GetMapping("/admins")
-    public String pageAdmins(Model model) {
-
-
-
-        List<PetViewModel> treatments = this.petService.findPets(TypeOfHelpEnumName.TREATMENT);
-        List<PetViewModel> grooms = this.petService.findPets(TypeOfHelpEnumName.GROOMING);
-        List<PetViewModel> hotels = this.petService.findPets(TypeOfHelpEnumName.HOTEL);
-        List<PetViewModel> schools = this.petService.findPets(TypeOfHelpEnumName.SCHOOL);
-        List<PetViewModel> preventions = this.petService.findPets(TypeOfHelpEnumName.PREVENTION);
-        Long countOfPets = this.petService.countAllPets();
-
-
-        model.addAttribute("treatments", treatments);
-        model.addAttribute("grooms", grooms);
-        model.addAttribute("hotels", hotels);
-        model.addAttribute("schools", schools);
-        model.addAttribute("preventions", preventions);
-        model.addAttribute("countOfPets", countOfPets);
-
-
-
-        return "pages/admins";
-    }
-
 
     @GetMapping("/{id}")
     public String getPageById(@PathVariable("id") Long id) {
